fa23d7dc4afc7a8da8cdfb5f36d9b7555dc426e3
vishnupv863 <vishnupv863@gmail.com>
2026-09-25 17:42:44 +0530

    docker has added

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://react-frontend.ambitiousbeach-dced774b.centralindia.azurecontainerapps.io",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
